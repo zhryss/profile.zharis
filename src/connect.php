aae3a59cc4b1900502efde4c28e88dcab763605b
zhryss <zharis211206@gmail.com>
2024-10-10 00:07:24 +0700

    Add files via upload

--- src/connect.php
+++ src/connect.php
@@ -2,10 +2,4 @@
 // Koneksi ke database Supabase
 $conn = pg_connect("host=wokunjzwnzfvgtvoccmg.supabase.co dbname=postgres user=postgres password=Zharispemilos21");
 
-// Cek koneksi
-if (!$conn) {
-    die("Connection failed: " . pg_last_error());
-} else {
-    echo "Koneksi berhasil!";
-}
 ?>
