502fd689540b5b63b05b675c748407f5b87903bb
zhryss <zharis211206@gmail.com>
2024-10-10 00:03:36 +0700

    Delete PEMILOS 2024 directory

--- PEMILOS 2024/src/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Koneksi ke database Supabase
-$conn = pg_connect("host=aws-0-ap-southeast-1.pooler.supabase.com dbname=postgres user=postgres.wokunjzwnzfvgtvoccmg password=Zharispemilos21");
-
-// Cek koneksi
-if (!$conn) {
-    die("Connection failed: " . pg_last_error());
-}
-?>
